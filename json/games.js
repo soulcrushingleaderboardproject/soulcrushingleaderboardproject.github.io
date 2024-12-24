2add3fb84c0c8c1dd9971224a1bea324f2a0a1ab
soulcrushingleaderboardproject <soulcrushingleaderboardproject@outlook.com>
2024-12-24 11:02:30 -0500

    Add files via upload

---
+++ json/games.js
@@ -0,0 +1,19 @@
+var all_games = [
+  {"abbr": "AHoSCT", "name": "Azi's House of Soul Crushing Towers", "link": "https://www.roblox.com/games/83360243495088/ToPD-ToNCL-Azis-House-of-Soul-Crushing-Towers"},
+  {"abbr": "AToBM", "name": "Andr's Towers of Bruh Moments", "link": "https://www.roblox.com/games/2830417603/anothers-towers"},
+  {"abbr": "AToS", "name": "Another's Towers of Stupidity", "link": "https://www.roblox.com/games/2830417603/anothers-towers"},
+  {"abbr": "ATS", "name": "Amazing's Tower Supernerfs", "link": "https://www.roblox.com/games/17120699596/Amazings-Tower-Supernerfs"},
+  {"abbr": "CSCD", "name": "Caleb's Soul Crushing Domain", "link": "https://www.roblox.com/games/10283991824/SCOPE-3-Calebs-Soul-Crushing-Domain"},
+  {"abbr": "DEToH", "name": "Democratically Elected Towers of Hell", "link": "https://www.roblox.com/games/116948649505593/Alpha-DEToH"},
+  {"abbr": "JToH", "name": "Juke's Towers of Hell", "link": "https://www.roblox.com/games/8562822414/Jukes-Towers-of-Hell"},
+  {"abbr": "JXL", "name": "JToH XL Project", "link": "https://www.roblox.com/games/13218032675/JToH-XL"},
+  {"abbr": "JXXL", "name": "JToH XXL Project", "link": "https://www.roblox.com/games/17846926884/RING-1-UPDATE-3-5-4-JToH-XXL"},
+  {"abbr": "KToN", "name": "Kirk's Towers of Nonsense", "link": "https://www.roblox.com/games/7184682048/QoL-SECTION-3-Kirks-Towers-of-Nonsense"},
+  {"abbr": "LToD", "name": "Lask's Towers of Dread", "link": "https://www.roblox.com/games/18386437234/lasks-towers-of-dread"},
+  {"abbr": "NCoH", "name": "Nox's Citadels of Hell", "link": "https://www.roblox.com/games/9237964584/Noxs-Citadels-of-Hell"},
+  {"abbr": "PToH", "name": "Purist Towers of Hell", "link": "https://www.roblox.com/games/7593639579/RING-5-Purist-Towers-of-Hell"},
+  {"abbr": "TBP", "name": "The Baseplate Project", "link": "https://www.roblox.com/games/15957446104/EVILPLATE-The-Baseplate-Project"},
+  {"abbr": "TCT", "name": "Thai's Crazy Towers", "link": "https://www.roblox.com/games/9066198427/Thais-Crazy-Towers"},
+  {"abbr": "TWR$", "name": "Tower$", "link": "https://www.roblox.com/games/13594795843/Tower"},
+  {"abbr": "ZToA", "name": "Zee's Towers of Anger", "link": "https://www.roblox.com/games/5630198391/ToCUP-Zees-Towers-of-Anger-Reborn"}
+]
