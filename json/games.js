9b5a94e2088f5185b5a86c4a39e42e5eecb0cfa0
soulcrushingleaderboardproject <soulcrushingleaderboardproject@outlook.com>
2025-01-20 20:09:55 -0500

    Update games.js

--- json/games.js
+++ json/games.js
@@ -1,12 +1,15 @@
 var all_games = [
   {"abbr": "AHoSCT", "name": "Azi's House of Soul Crushing Towers", "link": "https://www.roblox.com/games/83360243495088/ToPD-ToNCL-Azis-House-of-Soul-Crushing-Towers"},
+  {"abbr": "ATM", "name": "Anno's Tantrum Machine", "link": "https://www.roblox.com/games/14155809913/Annos-Tantrum-Machine"},
   {"abbr": "AToBM", "name": "Andr's Towers of Bruh Moments", "link": "https://www.roblox.com/games/11300875374/CHRISTMAS-Andrs-Towers-of-Bruh-Moments"},
+  {"abbr": "AToH", "name": "Aspetic's Towers of Hardness", "link": "https://www.roblox.com/games/5902053520/Aspetics-Tower-of-Hardness"},
   {"abbr": "AToS", "name": "Another's Towers of Stupidity", "link": "https://www.roblox.com/games/2830417603/anothers-towers"},
   {"abbr": "ATS", "name": "Amazing's Tower Supernerfs", "link": "https://www.roblox.com/games/17120699596/Amazings-Tower-Supernerfs"},
   {"abbr": "CKR", "name": "Crazy's Kaizo Regions", "link": "https://www.roblox.com/games/3103266746/Crazys-Kaizo-Regions"},
   {"abbr": "CSCD", "name": "Caleb's Soul Crushing Domain", "link": "https://www.roblox.com/games/10283991824/SCOPE-3-Calebs-Soul-Crushing-Domain"},
   {"abbr": "CTT", "name": "Community Top Towers", "link": "https://www.roblox.com/games/109970721198777/Domain-1-LETS-GO"},
   {"abbr": "DEToH", "name": "Democratically Elected Towers of Hell", "link": "https://www.roblox.com/games/116948649505593/Alpha-DEToH"},
+  {"abbr": "EAT", "name": "Eloi's Artistic Towers", "link": "https://www.roblox.com/games/10192639557/Elois-Artistic-Towers"},
   {"abbr": "HT", "name": "Hades Towers", "link": "https://www.roblox.com/games/121125135868314/Hades-Towers-CHRISTMAS"},
   {"abbr": "JToH", "name": "Juke's Towers of Hell", "link": "https://www.roblox.com/games/8562822414/Jukes-Towers-of-Hell"},
   {"abbr": "JXL", "name": "JToH XL", "link": "https://www.roblox.com/games/13218032675/JToH-XL"},
@@ -24,6 +27,7 @@ var all_games = [
   {"abbr": "SMToH", "name": "Shark's Mini Towers of Hecc", "link": "https://www.roblox.com/games/16057377099/Sharks-Mini-Towers-of-Hecc"},
   {"abbr": "TBP", "name": "The Baseplate Project", "link": "https://www.roblox.com/games/15957446104/EVILPLATE-The-Baseplate-Project"},
   {"abbr": "TCT", "name": "Thai's Crazy Towers", "link": "https://www.roblox.com/games/9066198427/Thais-Crazy-Towers"},
+  {"abbr": "TCTA", "name": "The Classic Tower Archive", "link": "https://www.roblox.com/games/14377924053/The-Classic-Tower-Archive-CHRISTMAS-EVENT"},
   {"abbr": "TG", "name": "Tower Game", "link": "https://www.roblox.com/games/5976062696/tower-game"},
   {"abbr": "TWR$", "name": "Tower$", "link": "https://www.roblox.com/games/13594795843/Tower"},
   {"abbr": "ZToA", "name": "Zee's Towers of Anger", "link": "https://www.roblox.com/games/5630198391/ToCUP-Zees-Towers-of-Anger-Reborn"}
