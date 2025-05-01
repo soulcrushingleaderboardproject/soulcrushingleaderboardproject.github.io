afde5088e5f56891f575648b8a5c557a31b2c4a2
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-04-30 22:46:04 -0700

    no cache

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, send_from_directory
+import os
 
 app = Flask(__name__)
+
+@app.after_request
+def add_no_cache_headers(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/static/<path:filename>")
+def static_files(filename):
+    response = make_response(send_from_directory(os.path.join(app.root_path, 'static'), filename))
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)
