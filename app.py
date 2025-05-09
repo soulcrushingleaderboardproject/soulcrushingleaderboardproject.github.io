5641919de792934c6b4f833bf71e6da399dcc7f5
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-05-09 00:43:36 -0700

    first collection of table info

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, make_response, send_from_directory
 import os
+from bs4 import BeautifulSoup
+import csv
+import requests
 
 app = Flask(__name__)
 
@@ -25,6 +28,13 @@ def static_files(filename):
 @app.route("/favicon.ico")
 def favicon():
     return app.send_static_file("sclp.png")
+
+html = requests.get('https://docs.google.com/spreadsheets/d/1ffz-IFNSEDQay9jkR5JbOj7NPEljBX4jc2oIYzypRLc/edit?gid=0#gid=0').text
+soup = BeautifulSoup(html, "lxml")
+table = soup.find_all("table")[0]
+with open("0.csv", "w") as f:
+    wr = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+    wr.writerows([[td.text for td in row.find_all("td")] for row in table.find_all("tr")])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)
