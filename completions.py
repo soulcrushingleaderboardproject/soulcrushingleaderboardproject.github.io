3981de049549a28a343d78c1ccfe3ad028730359
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-05-11 16:16:44 -0700

    load completions data

---
+++ completions.py
@@ -0,0 +1,23 @@
+import os
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+
+def format(data):
+    pairs = {}
+    for row in data:
+        pairs[row[0]] = row[1]
+            
+    return pairs
+
+API_KEY = os.getenv("GOOGLE_SHEETS_API_KEY")
+SHEET_ID = "1ffz-IFNSEDQay9jkR5JbOj7NPEljBX4jc2oIYzypRLc"
+RANGE = "comps!A:B"
+
+url = f"https://sheets.googleapis.com/v4/spreadsheets/{SHEET_ID}/values/{RANGE}?key={API_KEY}"
+response = requests.get(url)
+values = response.json().get("values", [])
+
+data = [row for row in values if any(row)]
+data = format(data)
+print(data)
