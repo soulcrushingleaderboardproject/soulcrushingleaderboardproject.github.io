8e5adb1c37b57215374fd029147dc8974720ef95
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-05-11 16:25:57 -0700

    fixed data formatting

--- completions.py
+++ completions.py
@@ -3,22 +3,23 @@ import requests
 from dotenv import load_dotenv
 load_dotenv()
 
-def format(data):
+def format(data, key):
     pairs = {}
     for row in data:
-        pairs[row[0]] = row[1:]
-            
+        if len(row) > key:
+            value = row[:key] + row[key+1:]
+            pairs[row[key]] = value
     return pairs
 
-def get_data(range):
+def get_data(range, key):
     response = requests.get(f"https://sheets.googleapis.com/v4/spreadsheets/1ffz-IFNSEDQay9jkR5JbOj7NPEljBX4jc2oIYzypRLc/values/{range}?key={os.getenv("GOOGLE_SHEETS_API_KEY")}")
     values = response.json().get("values", [])
 
     data = [row for row in values if any(row)]
-    return format(data)
+    return format(data, key)
 
-data = get_data("comps!A:B")
-towers = get_data("towers!A:D")
+data = get_data("comps!A:B", 0)
+towers = get_data("towers!A:D", 1)
 print(towers)
 
 with open("edit.txt", "r") as f:
@@ -28,4 +29,4 @@ with open("edit.txt", "r") as f:
             #user exists
             pass
 
-# print(data)
+print(data)
