108980087c452eaed9adaaa2fa9b10ab2f989fbd
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-06-22 21:19:55 -0700

    improve get_data function with better formatting

--- app.py
+++ app.py
@@ -13,26 +13,36 @@ def add_no_cache_headers(response):
     response.headers['Expires'] = '0'
     return response
 
-def format_data(x):
-    res = []
-    for row in x:
-        res.append({"name": row[0], "completions": list(map(int, row[1].split(",")))})
-    return res
+def get_data(r):
+    url = f"https://sheets.googleapis.com/v4/spreadsheets/1ffz-IFNSEDQay9jkR5JbOj7NPEljBX4jc2oIYzypRLc/values/{r}?key={os.getenv("GOOGLE_SHEETS_API_KEY")}"
+    
+    response = requests.get(url).json()
+    values = response.get("values", [])
+    if len(values) < 2:
+        return []
 
-API_KEY = os.getenv("GOOGLE_SHEETS_API_KEY")
-SHEET_ID = "1ffz-IFNSEDQay9jkR5JbOj7NPEljBX4jc2oIYzypRLc"
-RANGE = "comps!A:B"
+    headers = values[0]
+    data = []
 
-url = f"https://sheets.googleapis.com/v4/spreadsheets/{SHEET_ID}/values/{RANGE}?key={API_KEY}"
-response = requests.get(url)
-values = response.json().get("values", [])
+    for row in values[1:]:
+        item = {}
+        for i, header in enumerate(headers):
+            val = row[i] if i < len(row) else ""
+            if header.lower() == "completions":
+                item[header] = [int(x) for x in val.split(",") if x] if val else []
+            else:
+                item[header] = val
+        data.append(item)
 
-data = [row for row in values if any(row)]
-data = format_data(data)
+    return data
+
+all_completions = get_data("backup!A:C")
+# towers = [row[0] for row in values if any(row)]
+print(all_completions)
 
 @app.route("/")
 def home():
-    return render_template("index.html", data=data)
+    return render_template("index.html", all_completions=all_completions)
 
 @app.route("/static/<path:filename>")
 def static_files(filename):
